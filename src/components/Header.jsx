1622e102719c1951eb500ef5c340908614e413dd
BrandonDoesEmails <blester1989@gmail.com>
2023-10-19 22:54:47 -0400

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import logo from '../assets/logo.png';
 const Header = () => {
   return (
     <header>
+        <Link to='/'>
         <div className='logo'>
-          <Link to='/'>
             <img src={logo} alt="Shopping Logo" />
             <span>Shopping Cart</span>
-          </Link>
         </div>
+        </Link>
         <nav className='navigation'>
             <NavLink className='link' to='/' end>Home</NavLink>
             <NavLink className='link' to='/cart'>Cart</NavLink>
